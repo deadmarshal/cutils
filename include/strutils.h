4d0b90326139ee71e98309e687415bd8dce27216
deadmarshal <adeadmarshal@gmail.com>
2021-08-21 22:44:04 +0430

    added trim() and trim_copy() functions.

--- include/strutils.h
+++ include/strutils.h
@@ -6,12 +6,15 @@
 #include<assert.h>
 #include<stddef.h>
 #include<ctype.h>
-#include<stdio.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+void trim(char* str);
+void trim_left(char* str);
+void trim_right(char* str);
+const char* trim_copy(const char* str);
 const char* trim_left_copy(const char* str);
 const char* trim_right_copy(const char* str);
 
@@ -24,15 +27,18 @@ const char* trim_right_copy(const char* str);
 ///////////////IMPLEMENTATION PART///////////////
 #ifdef STRUTILS_IMPLEMENTATION
 
+void trim(char* str)
+{
+  trim_left(str);
+  trim_right(str);
+}
+
 void trim_left(char* str)
 {
   size_t idx = {0};
-  // Skip spaces.
   do{idx++;}while(isspace(str[idx]));
   char* last = &str[strlen(str)];
-
-  // Subtraction of ptr to the last character from the first character of the
-  // string gives use the size for allocation.
+  
   ptrdiff_t sz = last - str;
   char* s = realloc(str, sz+1);
   assert(s && "Memory allocation failed.\n");
@@ -49,12 +55,10 @@ void trim_left(char* str)
 
 void trim_right(char* str)
 {
-  // Ptr to last character (before NUL-terminator).
   char* last = str + strlen(str) - 1;
-  // skip spaces downwards.
   do{last--;}while(isspace(*last));
   ptrdiff_t sz = last - str + 1;
-  
+
   char* s = realloc(str, sz+1);
   assert(s && "Memory allocation failed.\n");
   
@@ -66,6 +70,16 @@ void trim_right(char* str)
     }
   s[i] = '\0';
   str = s;
+}
+
+const char* trim_copy(const char* str)
+{
+  char* s = strdup(str);
+  assert(s && "Memory allocation failed.\n");
+  trim_left(s);
+  trim_right(s);
+  
+  return (const char*)s;
 }
 
 const char* trim_left_copy(const char* str)
@@ -100,7 +114,7 @@ const char* trim_right_copy(const char* str)
   while(last > str && isspace(*last)){last--;};
   // Size of actual string.
   size_t sz = (last - str) + 1;
-
+  
   char* s = malloc((sz+1) * sizeof(char));
   assert(s && "Memory allocation failed.\n");
   
@@ -115,5 +129,4 @@ const char* trim_right_copy(const char* str)
   return (const char*)s;
 }
 
-#endif // STRUTILS_IMPLEMENTATION
-
+#endif
